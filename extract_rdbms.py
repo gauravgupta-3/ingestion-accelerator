74e89896bb934db9e4e6ddd146c512742ed2bb6e
Gaurav Gupta (HM0010813) <gaurav.gupta3@happiestminds.com>
2025-04-07 20:52:54 +0530

    Initial Commit

---
+++ extract_rdbms.py
@@ -0,0 +1,108 @@
+import pyodbc
+from sqlalchemy import create_engine, text
+import datetime
+import pandas as pd
+import logging
+from dateutil import parser
+from utils.db_Util.dbUtil import sql_server_connect
+from utils.s3_Util.s3Util import s3_delete_file,s3_upload_file_parquet
+from config.config import read_config
+from prefect import flow, task
+
+logger = logging.getLogger()
+logger.setLevel(level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.precision", 3)
+
+
+
+
+@flow(log_prints=True)
+def extract_rdbms():
+    ##Read configuration variables
+    config_data = read_config()
+    driver = config_data['driver']
+    host = config_data['host']
+    metadata_database = config_data['metadata_database']
+    metadata_rdbms_table = config_data['metadata_rdbms_table']
+    ##Read dbms metadata
+    cnxn_metadata = sql_server_connect(driver,host,metadata_database)
+    sql_query_metadata = f"select * from {metadata_rdbms_table}"
+    metadata_df = pd.read_sql(sql_query_metadata,cnxn_metadata)
+    print("Metadata df:\n {}".format(metadata_df.to_string()))
+    metadata_df['delay_in_seconds'] = metadata_df['delay_in_seconds'].fillna(0)
+    for row in metadata_df.itertuples():
+        if row.active_flag == 1 and row.src_type =='sql_server':
+        ##Incremental load
+            if row.load_type == 'incremental':
+                file_name = row.src_table_name
+                r_watermark_value = row.watermark_value - datetime.timedelta(seconds=row.delay_in_seconds)
+                sql_query =f"select * from {row.src_table_name} where {row.watermark_column}>cast('{r_watermark_value}' as datetime2)"
+                logger.info("Start incremental extraction from SQL server for table: {}".format(file_name))
+                cnxn=sql_server_connect(driver,host,row.src_database)
+                sql_watermark = f"select max({row.watermark_column}) as c1 from {row.src_table_name}" 
+                sql_df = pd.read_sql(sql_query,cnxn)
+                if not sql_df.empty:
+                    watermark_updated_timestamp = pd.read_sql(sql_watermark,cnxn)
+                    watermark_updated = watermark_updated_timestamp.iloc[0]['c1']
+                    watermark_updated_n = watermark_updated+datetime.timedelta(milliseconds=5)
+                    
+                    file_timestamp = datetime.datetime.now().replace(microsecond = 0)  
+                    #update statement
+                    #cursor = cnxn_metadata.cursor()
+                    update_sql = text(f'UPDATE {metadata_rdbms_table} SET watermark_value= :x WHERE object_id= :y')
+                    val = {'x': watermark_updated_n, 'y':row.object_id} 
+                    logger.info("Updated SQL {}".format(update_sql))
+                    logger.info("VAL {}".format(val))
+                    cnxn_metadata.execute(update_sql,val)
+                    cnxn_metadata.commit()
+                    logger.info("Updated watermark value:{} in table {}".format(watermark_updated_n,file_name)) 
+                
+                    u_file_name = row.src_table_name+'_'+str(file_timestamp)
+                    extension = '.parquet'
+                    s3_key = row.s3_dest_folder +'/'+row.src_name+'/'+file_name+'/'+u_file_name+extension
+                    s3_upload_file_parquet(row.s3_dest_bucket_name,s3_key,sql_df,extension)
+                else:
+                    logger.info("No data extracted for this run for {} table for watermark value: {}".format(file_name,r_watermark_value))
+                logger.info("Finish incremental extraction from SQL server for table: {}".format(file_name))
+        ##Full load
+            elif row.load_type == 'full':
+                file_name = row.src_table_name
+                sql_query =f"select * from {row.src_table_name}"
+                logger.info("Start Full load extraction from SQL server for table: {}".format(file_name))
+                cnxn=sql_server_connect(driver,host,row.src_database)
+                sql_df = pd.read_sql(sql_query,cnxn)
+                #print(sql_df.to_string())
+                if sql_df.empty:
+                    logger.info("No data extracted for this run for {} table {}".format(file_name))
+                    
+                else: 
+                    file_name = row.src_table_name
+                    extension = '.parquet'
+                    s3_key = row.s3_dest_folder +'/'+row.src_name+'/'+file_name+'/'+file_name+extension
+                    s3_delete_file(row.s3_dest_bucket_name,s3_key)
+                    s3_upload_file_parquet(row.s3_dest_bucket_name,s3_key,sql_df,extension)
+                logger.info("Finish Full load extraction from SQL server for table: {}".format(file_name))
+        ##Snapshot load
+            elif row.load_type == 'snapshot':
+                file_name = row.src_table_name
+                sql_query =f"select * from {row.src_table_name}"
+                snapshot_timestamp = datetime.datetime.now().replace(microsecond = 0)
+                logger.info("Start Snapshot extraction from SQL server for table: {}".format(file_name))
+                cnxn=sql_server_connect(driver,host,row.src_database)
+                sql_df = pd.read_sql(sql_query,cnxn)
+                if sql_df.empty:
+                    logger.info("No data extracted for this run for {} table {}".format(file_name))
+                else: 
+                    u_file_name = row.src_table_name+'_'+str(snapshot_timestamp)
+                    extension = '.parquet'
+                    s3_key = row.s3_dest_folder +'/'+row.src_name+'/'+file_name+'/'+u_file_name+'_'+row.load_type+extension
+                    s3_upload_file_parquet(row.s3_dest_bucket_name,s3_key,sql_df,extension)
+                logger.info("Finish Snapshot extraction from SQL server for table: {}".format(file_name)) 
+
+if __name__ == "__main__":
+    extract_rdbms()
---
+++ extract_shared_files.py
@@ -0,0 +1,109 @@
+import os 
+import glob 
+import pandas as pd
+import boto3
+import pyarrow.parquet as pa
+import io
+import shutil
+import logging
+from utils.s3_Util.s3Util import s3_delete_file,s3_upload_file_parquet
+from utils.db_Util.dbUtil import sql_server_connect
+from config.config import read_config
+from prefect import flow, task
+
+logger = logging.getLogger()
+logger.setLevel(level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+@task
+def archive_local_src_file(local_file_path,source_archival_path,file_name):
+    try:
+        shutil.move(local_file_path,source_archival_path)
+        logger.info("File Successfully Archived: {file_name}")
+    except Exception as ex:
+        logger.error("File cannot be archived")
+        logger.error(ex)
+
+@flow(log_prints=True)
+def extract_shared_windows_file():
+    ##Read configuration variables
+    ##Read configuration variables
+    config_data = read_config()
+    driver = config_data['driver']
+    host = config_data['host']
+    metadata_database = config_data['metadata_database']
+    metadata_file_table = config_data['metadata_file_table']
+    ##Read dbms metadata
+    cnxn_metadata = sql_server_connect(driver,host,metadata_database)
+    sql_query_metadata = f"select * from {metadata_file_table}"
+    metadata_df = pd.read_sql(sql_query_metadata,cnxn_metadata)
+    print("Metadata df:\n {}".format(metadata_df.to_string()))
+    for row in metadata_df.itertuples():
+        if row.active_flag == 1 and row.src_type =='shared_windows_path':
+            for root, dirs, files in os.walk(row.src_file_path,topdown=True):
+                if files:
+                    for file_name in files:
+                        local_file_path = os.path.join(root, file_name)
+                        basename, extension = os.path.splitext(file_name)
+                        source_archive_path_dir = row.src_archive_path
+                        source_archival_full_path = os.path.join(source_archive_path_dir, file_name)
+                        
+                        isExist = os.path.exists(source_archive_path_dir)
+                        if not isExist:
+                            os.makedirs(source_archive_path_dir)
+
+                        s3_key = row.s3_dest_folder +'/'+row.src_name+'/'+file_name
+                        #print("Filename", file_name)
+                        #print("S3 key:", s3_key)
+                        if extension =='.csv':
+                            with open(local_file_path) as f:
+                                encode = str(f).split("encoding=",1)[1][:-1]
+                                df = pd.read_csv(local_file_path, encoding=encode)
+                            logger.info("Started CSV file extraction {}".format(file_name))                         
+                            s3_upload_file_parquet(row.s3_dest_bucket_name,s3_key,df,extension)
+                            archive_local_src_file(local_file_path,source_archival_full_path,file_name)
+                            logger.info("Finshed CSV file extraction {}".format(file_name))  
+                                
+                        elif extension =='.json':
+                            df = pd.read_json(local_file_path)
+                            logger.info("Started Json file extraction {}".format(file_name))                          
+                            s3_upload_file_parquet(row.s3_dest_bucket_name,s3_key,df,extension)
+                            archive_local_src_file(local_file_path,source_archival_full_path,file_name)
+                            logger.info("Finished Json file extraction {}".format(file_name))  
+
+                        elif extension =='.txt':
+                            df = pd.read_csv(local_file_path,sep=" ")
+                            logger.info("Started Text file extraction {}".format(file_name))                      
+                            s3_upload_file_parquet(row.s3_dest_bucket_name,s3_key,df,extension)
+                            archive_local_src_file(local_file_path,source_archival_full_path,file_name)
+                            logger.info("Finished Text file extraction {}".format(file_name))  
+
+                        elif extension =='.parquet':
+                            table = pa.read_table(local_file_path)
+                            df = table.to_pandas()
+                            logger.info("Started Parquet file extraction {}".format(file_name)) 
+                            s3_upload_file_parquet(row.s3_dest_bucket_name,s3_key,df,extension)
+                            archive_local_src_file(local_file_path,source_archival_full_path,file_name)
+                            logger.info("Finished Parquet file extraction {}".format(file_name)) 
+                else:
+                    logger.info("File does not exist at path {}".format(row.src_file_path))
+
+
+if __name__ == "__main__":
+    extract_shared_windows_file()
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
