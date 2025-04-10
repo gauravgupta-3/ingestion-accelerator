0677934dbabbbe15fb921656df98e8be9188bbf3
Gaurav Gupta (HM0010813) <gaurav.gupta3@happiestminds.com>
2025-04-10 20:08:23 +0530

    requirements file

--- deployment/deployment_extract_rdbms.py
+++ deployment/deployment_extract_rdbms.py
@@ -1,7 +1,16 @@
 from prefect import flow
+from pathlib import Path
 
 # Source for the code to deploy (here, a GitHub repo)
 SOURCE_REPO="https://github.com/gauravgupta-3/ingestion-accelerator.git"
+
+# This helper function reads the requirements.txt file.
+def read_requirements(file_path="requirements.txt"):
+    """Read and parse requirements.txt file"""
+    requirements = Path(file_path).read_text().splitlines()
+    # Filter out empty lines and comments.
+    return [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
+
 
 if __name__ == "__main__":
     flow.from_source(
@@ -10,5 +19,6 @@ if __name__ == "__main__":
     ).deploy(
         name="extract-rdbms-deployment",
         work_pool_name="my-managed-pool",
+        job_variables={"pip_packages": read_requirements()},
         cron="0 * * * *",  # Run every hour
     )
